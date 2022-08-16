725b28658db95ed07fa54070f7f04fb71c921721
Andres Maragua <cristian.carrillo@controlt.com.co>
2022-08-16 03:15:53 -0500

    edit home page

---
+++ lib/views/home/home.dart
@@ -0,0 +1,6 @@
+export 'package:movies_app/bloc/home/home.dart';
+export 'package:movies_app/commons/constants/constants.dart';
+export 'package:movies_app/commons/widgets/widgets.dart';
+export 'package:movies_app/views/home/widgets/bottom_navigation.dart';
+export 'package:movies_app/views/home/widgets/popular.dart';
+export 'package:movies_app/views/home/widgets/recommendations.dart';
--- lib/views/home/pages/home_page.dart
+++ lib/views/home/pages/home_page.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:movies_app/views/home/home.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocProvider<HomeBloc>(
+      create: (_) => HomeBloc(homeRepository: context.read<HomeRepository>())
+        ..add(HomeStarted()),
+      child: Scaffold(
+        backgroundColor: Constants.secondColor,
+        appBar: const CustomAppBar(title: 'Home'),
+        bottomNavigationBar: const BottomNavigation(),
+        body: SafeArea(
+          child: BlocBuilder<HomeBloc, HomeState?>(
+            builder: (context, state) {
+              if (state is HomeLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (state is GetList) {
+                return SingleChildScrollView(
+                  child: Container(
+                    margin: const EdgeInsetsDirectional.only(
+                        bottom: 20, start: 20, end: 20),
+                    child: Column(
+                      children: [
+                        Popular(popular: state.popular),
+                        const Divider(
+                            color: Constants.thirdColor,
+                            thickness: 2,
+                            height: 40),
+                        Recommendations(recommendations: state.recommendations),
+                      ],
+                    ),
+                  ),
+                );
+              }
+              return Container();
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
