7ad79c8836e2f1be70fdc8abaca628929e43e5a9
Andres Maragua <cristian.carrillo@controlt.com.co>
2022-08-16 03:13:53 -0500

    edit constants

--- lib/commons/constants/constants.dart
+++ lib/commons/constants/constants.dart
@@ -7,4 +7,8 @@ class Constants {
   static const primaryColor = Color(0xFFFFD233);
   static const secondColor = Color(0xFF191919);
   static const thirdColor = Color(0xFF8C8C8C);
+
+  //Host
+  static const host = 'https://api.themoviedb.org/3/tv';
+  static const apiKey = '?api_key=1fe268929a9d2cc8a5f5a489fde916c8';
 }
